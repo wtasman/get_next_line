4cfd3d28c5913d89f8aede8db9dd02f0b9d583f4
William Asman <wasman@e1z2r10p9.42.us.org>
2016-11-10 17:03:18 -0800

    removed random p

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: wasman <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/21 09:25:16 by wasman            #+#    #+#             */
-/*   Updated: 2016/11/10 13:17:51 by wasman           ###   ########.fr       */
+/*   Updated: 2016/11/10 17:02:19 by wasman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 32
-p
 # include "libft/libft.h"
 
 int	get_next_line(const int fd, char **line);
