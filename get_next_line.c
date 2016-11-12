73c40842c6372f02665b7b228abaff75f7d91703
William Asman <wasman@e1z2r9p17.42.us.org>
2016-11-12 09:57:42 -0800

    add a find newline that works

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,25 @@
 /*   By: wasman <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/19 17:29:35 by wasman            #+#    #+#             */
-/*   Updated: 2016/11/10 16:58:45 by wasman           ###   ########.fr       */
+/*   Updated: 2016/11/12 09:56:45 by wasman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int		find_n(char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (0);
+}
 
 char	*read_in(int fd)
 {
@@ -20,11 +34,10 @@ char	*read_in(int fd)
 	buff = memset(buff, 0, BUFF_SIZE);
 	len = read(fd, buff, BUFF_SIZE);
 	if (len <= 0)
-		return (0);
+		return (NULL);
 	buff[BUFF_SIZE] = '\0';
 	return (buff);
 }
-
 
 int	get_next_line(const int fd, char **line)
 {
